ffdf226689b1fed5d09d8c42e9de64bc2e146a4a
Samy Oubouaziz <samy.oubouaziz@gmail.com>
2026-04-10 07:55:13 +0200

    add frontmatter

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import mdx from '@mdx-js/rollup'
+import remarkFrontmatter from 'remark-frontmatter'
+import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
 
 export default defineConfig({
   plugins: [
-    mdx(),   // MDX files will be treated as React components
+    mdx({
+      remarkPlugins: [remarkFrontmatter, [remarkMdxFrontmatter, { name: 'meta' }]],
+    }),
     react(),
   ],
 })
